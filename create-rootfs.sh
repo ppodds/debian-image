bdc2546dbb5bd1811290a5c780fdf548ab76c2a5
ppodds <oscar20020629@gmail.com>
2023-07-08 02:48:23 +0800

    fix: script permission issue

--- create-rootfs.sh
+++ create-rootfs.sh
@@ -5,8 +5,8 @@ ARCH=$2
 VERSION=$3
 
 usage() {
-    echo 'Usage: sudo ./create-rootfs.sh <suite> <arch> <version>'
-    echo 'Example: sudo ./create-rootfs.sh etch amd64 20100619T041712Z'
+    echo 'Usage: ./create-rootfs.sh <suite> <arch> <version>'
+    echo 'Example: ./create-rootfs.sh etch amd64 20100619T041712Z'
     echo 'You can find available version at https://snapshot.debian.org/archive/debian/'
     exit 1
 }
@@ -21,18 +21,23 @@ ROOTFS_DIR="$DIR/rootfs"
 # Clean folder if already existed
 if [ -d $ROOTFS_DIR ]; then 
     echo 'Old files detected, remove old files.'
-    rm -rf $ROOTFS_DIR
+    sudo rm -rf $ROOTFS_DIR
+    sudo rm -f $DIR/rootfs.tar.gz
 fi
 
 mkdir -p $ROOTFS_DIR
 
 # Create root fs
-debootstrap --arch=$ARCH $SUITE $ROOTFS_DIR "https://snapshot.debian.org/archive/debian/${VERSION}/" 
+sudo debootstrap --arch=$ARCH $SUITE $ROOTFS_DIR "http://snapshot.debian.org/archive/debian/${VERSION}/" 
 if [ $? -ne 0 ]; then
     echo 'debootstrap failed!'
     exit 1
 fi
 
 # Create tarball
-tar cpzf "$DIR/rootfs.tar.gz" $ROOTFS_DIR
-
+echo 'Create tarball...'
+cd $ROOTFS_DIR
+sudo tar cpzf ../rootfs.tar.gz .
+sudo chown $USER:$USER ../rootfs.tar.gz
+cd $PWD
+echo 'Done!'
